77226aae9ef67e0bf6a9255e7286d4913574ce72
VÕ THIỆN TÍN <vothientin@gmail.com>
2021-06-03 00:16:21 +0700

    upload Image to Firebase

--- online-academy-frontend/src/components/app/App.js
+++ online-academy-frontend/src/components/app/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import Home from '../home/Home';
-
+import DetailPage from '../detail_page/Detail_page';
 function App() {
   return (
     <div className="App">
-      <Home/>
+      {/* <Home/> */}
+      <DetailPage/>
     </div>
   );
 }
