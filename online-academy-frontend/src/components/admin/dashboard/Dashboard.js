9d6f7b276db7a81c8bf5ca0bb11dbc55b308eb6d
LeTriThong <lttvip1999@gmail.com>
2021-06-14 00:42:43 +0700

    [Add] Admin page - Category demo

---
+++ online-academy-frontend/src/components/admin/dashboard/Dashboard.js
@@ -0,0 +1,15 @@
+import { Navbar, Nav, NavItem, Button, Glyphicon, NavDropdown, Form, FormControl } from 'react-bootstrap';
+
+import React, { useState } from 'react';
+import AdminHeader from '../common/AdminHeader';
+
+
+export default function Dashboard(props) {
+    return (
+        <div>
+            <AdminHeader/>
+        </div>
+    )
+}
+
+
